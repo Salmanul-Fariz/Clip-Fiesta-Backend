37ea0a4faef1426ae5ff9f407b6ded59f2d454a5
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-01-24 17:12:08 +0530

    created gatway

--- user_service/src/index.js
+++ user_service/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(8001, () => {
-  console.log('Started');
+  console.log('User Service is listening to port 8001');
 });
